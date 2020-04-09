6da14bea0bf7579589280fed0873bbde1bc7f3f8
your name here <your_email_here>
2020-04-08 22:32:18 -0400

    hopedully still good...? after using rollback...

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: videos
+#
+#  id          :bigint           not null, primary key
+#  genre_id    :integer          not null
+#  title       :string           not null
+#  description :text             not null
+#  video_url   :string           not null
+#  duration    :integer          not null
+#  year        :integer          not null
+#  views       :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Video < ApplicationRecord
+    validates :title, :description,:year, :duration, presence: true
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: videos
+#
+#  id          :bigint           not null, primary key
+#  genre_id    :integer          not null
+#  title       :string           not null
+#  description :text             not null
+#  video_url   :string           not null
+#  duration    :integer          not null
+#  year        :integer          not null
+#  views       :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require 'test_helper'
 
 class VideoTest < ActiveSupport::TestCase
