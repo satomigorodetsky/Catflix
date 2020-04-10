8ee77a7635430630e0580afd630d27ea32d22942
your name here <your_email_here>
2020-04-09 23:40:20 -0400

    middle of showing videos

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,6 @@ v1 = Video.create!({title:"Garfield: The Movie", description:"Jon Arbuckle buys 
 ",year:2004 , video_type:"movie", duration:90, views:100})
 
 
-v1.url.attach(io: open("https://catflix-dev.s3.amazonaws.com/garfield2004.mp4"), filename: "garfield2004.mp4" )
+# v1.url.attach(io: open("https://catflix-dev.s3.amazonaws.com/garfield2004.mp4"), filename: "garfield2004.mp4" )
 # v1.url.attach(io: File.open("/Users/s.okada/Desktop/contents/garfield2004.mp4"), filename: "garfie
 # ld2004")
